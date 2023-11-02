e82ecb3c4e1a657b72fa9a9ae7f9802fa6f43f72
manzicelse <149612563+manzicelse@users.noreply.github.com>
2023-11-02 20:08:17 -0100

    Add files via upload

---
+++ kjj.java
@@ -0,0 +1,17 @@
+public class kjj 
+{
+    public static void main(String[] args)
+    {
+    int a=20;
+    int b=15;
+    if(a>b)
+    {
+        System.out.println("a is great");
+    }
+            else
+            {
+                System.out.println("b is great");
+            }
+        }
+}
+
